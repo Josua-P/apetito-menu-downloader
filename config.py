cadacbe7a88b02ce15594a1da6b688f506895a8d
Josua-P <115071746+Josua-P@users.noreply.github.com>
2026-07-15 09:01:12 +0200

    Commented new config option

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ monthEmpty = "&nbsp;&nbsp;&nbsp;"
 wdFormat = "%a" # Format for the weekday.
 tsTimeFormat = "Aktualisiert am %d.%m. um %X." # Format for the timestamp in the plan.
 
-noDisplay=[]
+noDisplay=[] # Menu Items that shouldn't be displayed; e.g. ["Drink of choice", "Dessert"]
 
 language = 'en_US.utf8' # Language setting compliant to POSIX Standard (e.g. "en_US.utf8" -> English)
 
